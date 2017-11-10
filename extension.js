78b2aa7bd460a3c219a42f68088d80f45066af16
Ram Janarthan <ramj11@cs.utexas.edu>
2017-11-10 17:39:50 -0600

    Remove jquery dependency. Work on basics. TODO add youtube recognition

--- extension.js
+++ extension.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
 	var submitButton = document.getElementById('mybutton');
+	submitButton.onClick = onMyButtonClick();
 	var input = document.getElementById('input');
 });
 
 function onMyButtonClick(){
-	var input = document.getElementById('input');
-	var text = input.value;
-
+	//var input = document.getElementById('input');
+	//var text = input.value;
+	//console.log(" -- working :)");
+	window.alert("test");
 }
