ed22b8cd1525f4a943127f1376da5307bab71c30
ramjanarthan <ram.janarthan11@gmail.com>
2020-12-27 14:31:53 +0800

    Update extension to access the captions from browser storage and perform search

--- extension.js
+++ extension.js
@@ -1,12 +1,63 @@
-document.addEventListener('DOMContentLoaded', function() {
-	var submitButton = document.getElementById('mybutton');
-	submitButton.onClick = onMyButtonClick();
-	var input = document.getElementById('input');
+var captions = []
+
+window.addEventListener('click', function(_e) {
+	console.log("Click registered")
+    var target = _e.target;
+
+    switch(target.id) {
+		case 'mybutton':
+			let searchText = document.getElementById('input').value
+			let results = findClosestMatches(searchText)
+
+			if (results.length > 0) {
+				let times = results.reduce (function(prev, curr) {
+					return prev + " " + `${curr["startTime"]/1000} secs, `
+				}, "")
+				console.log(`Here are your best times: ${times}`)
+			} else {
+				console.log('Couldnt find any accurate results')
+			}
+	}
 });
 
-function onMyButtonClick(){
-	//var input = document.getElementById('input');
-	//var text = input.value;
-	//console.log(" -- working :)");
-	window.alert("test");
+/*
+
+CaptionSlice {
+    startTime: number,
+    duration: number,
+    text: string
+};
+
+*/
+function findClosestMatches(searchText) {
+	var result = []
+
+	if (searchText.length <= 0) {
+		return result
+	}
+
+	captions.forEach(function(captionSlice) {
+		if(captionSlice["text"].utf8.includes(searchText)) {
+			result.push(captionSlice)
+		}
+	})
+
+	return result
 }
+
+console.log("Hello YT")
+
+browser.storage.local.get()
+	.then(function(_data) {
+		let windowLocation = window.location.href
+
+		console.log(`Getting for captionSlices ${_data}`)
+		//console.log(`Trying to get data for : ${windowLocation}`)
+
+		if (_data["captionSlices"]) {
+			console.log(`Here's the data: ${_data["captionSlices"]}`)
+			captions = _data["captionSlices"]
+		} else {
+			console.log("Nothing found")
+		}
+	})
